692298c14fc2149aa5f4239638bcaed886943416
Colin Arpe <colin.j.arpe@outlook.com>
2017-01-14 23:36:34 -0500

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root to: "home#index"
+  root to: "home#index"
 
+  # This line throws a redundancy error if you un-comment it and run rails routes in the terminal
+  # Devise user routes are there without it
   # devise_for :users
+
+  resources :walkers, :services, :testimonials, :contents
 
 end
