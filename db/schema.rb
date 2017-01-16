bc271d6da48fa450cb2f019930583f492256074d
Colin Arpe <colin.j.arpe@outlook.com>
2017-01-16 13:21:44 -0500

    Closes #16.  Add role column to walker table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114133618) do
+ActiveRecord::Schema.define(version: 20170116181315) do
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "data_fingerprint"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
 
   create_table "contents", force: :cascade do |t|
     t.string   "page"
@@ -73,12 +86,13 @@ ActiveRecord::Schema.define(version: 20170114133618) do
     t.string   "lname"
     t.text     "bio"
     t.boolean  "active"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "role",                default: "Caregiver"
   end
 
 end
