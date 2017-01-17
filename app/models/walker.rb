f0e93a8895e9e273b1c4b186a5bbc86b327c904a
aisflat439 <fitzsimonsdevin@gmail.com>
2017-01-17 12:14:39 -0500

    card layout setup

--- app/models/walker.rb
+++ app/models/walker.rb
@@ -1,4 +1,9 @@
 class Walker < ApplicationRecord
 	has_attached_file :avatar
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+	def short_bio
+			bio.truncate(75)
+	end
+
 end
